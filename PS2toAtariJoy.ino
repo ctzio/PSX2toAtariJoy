42730e39d2801b654f8080275aa67360f6ea2d02
ctzio <ctzio@users.noreply.github.com>
2018-08-29 20:16:10 +0300

    Add files via upload

---
+++ PS2toAtariJoy.ino
@@ -0,0 +1,132 @@
+#include <PS2X_lib.h>  //for v1.6
+
+#define up 7
+#define down 6
+#define left 5
+#define right 4
+#define fire 2
+
+PS2X ps2x; // create PS2 Controller Class
+
+//right now, the library does NOT support hot pluggable controllers, meaning 
+//you must always either restart your Arduino after you conect the controller, 
+//or call config_gamepad(pins) again after connecting the controller.
+int error = 0; 
+byte type = 0;
+byte vibrate = 0;
+
+void setup(){
+ Serial.begin(57600);
+
+ //CHANGES for v1.6 HERE!!! **************PAY ATTENTION*************
+  
+ error = ps2x.config_gamepad(10,12,11,13, true, true);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+ 
+ if(error == 0){
+   Serial.println("Found Controller, configured successful");
+   Serial.println("Try out all the buttons, X will vibrate the controller, faster as you press harder;");
+  Serial.println("holding L1 or R1 will print out the analog stick values.");
+  Serial.println("Go to www.billporter.info for updates and to report bugs.");
+ }
+   
+  else if(error == 1)
+   Serial.println("No controller found, check wiring, see readme.txt to enable debug. visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 2)
+   Serial.println("Controller found but not accepting commands. see readme.txt to enable debug. Visit www.billporter.info for troubleshooting tips");
+   
+  else if(error == 3)
+   Serial.println("Controller refusing to enter Pressures mode, may not support it. ");
+   
+   //Serial.print(ps2x.Analog(1), HEX);
+   
+   type = ps2x.readType(); 
+     switch(type) {
+       case 0:
+        Serial.println("Unknown Controller type");
+       break;
+       case 1:
+        Serial.println("DualShock Controller Found");
+       break;
+       case 2:
+         Serial.println("GuitarHero Controller Found");
+       break;
+     }
+
+
+  //Setup Atari Joystick Pins
+
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  
+
+  
+}
+
+void loop(){
+   /* You must Read Gamepad to get new values
+   Read GamePad and set vibration values
+   ps2x.read_gamepad(small motor on/off, larger motor strenght from 0-255)
+   if you don't enable the rumble, use ps2x.read_gamepad(); with no values
+   
+   you should call this at least once a second
+   */
+  digitalWrite(2, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  ; 
+   
+   
+ if(error == 1) //skip loop if no controller found
+  return; 
+  
+ 
+ //DualShock Controller
+  
+    ps2x.read_gamepad(false, vibrate);          //read controller and set large motor to spin at 'vibrate' speed
+    
+    if(ps2x.Button(PSB_START))                   //will be TRUE as long as button is pressed
+         Serial.println("Start is being held");
+    if(ps2x.Button(PSB_SELECT))
+         Serial.println("Select is being held");
+         
+         
+     if(ps2x.Button(PSB_PAD_UP)) {         //will be TRUE as long as button is pressed
+       Serial.print("Up held this hard: ");
+       Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
+       digitalWrite(up, LOW);
+      }
+      if(ps2x.Button(PSB_PAD_RIGHT)){
+       Serial.print("Right held this hard: ");
+        Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
+        digitalWrite(right, LOW);
+      }
+      if(ps2x.Button(PSB_PAD_LEFT)){
+       Serial.print("LEFT held this hard: ");
+        Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
+        digitalWrite(left, LOW);
+      }
+      if(ps2x.Button(PSB_PAD_DOWN)){
+       Serial.print("DOWN held this hard: ");
+     Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
+     digitalWrite(down, LOW);
+      }   
+      if(ps2x.Button(PSB_BLUE)){
+       Serial.println("Fire held this hard: ");
+       digitalWrite(fire, LOW);
+     
+    
+      }   
+  
+
+ 
+ 
+ delay(50);
+     
+}
